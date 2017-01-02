fc99b0987ec60b937a5f40aefeb00b70ab45874b
Carla <carla.debeer.uk@gmail.com>
2017-01-02 20:25:36 +0000

    Adding closed-form solution to linear regression using the normal equations

---
+++ Multivariate Linear Regression/normalEqn.m
@@ -0,0 +1,11 @@
+% Function that computes the closed-form solution to linear 
+% regression using the normal equations.
+% Based on the Coursera 'Machine Learning' online course
+% Created: January 2017
+
+function [theta] = normalEqn(X, y)
+
+theta = zeros(size(X, 2), 1);
+theta = pinv(X'*X)*X'*y;
+
+end
