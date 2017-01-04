90dfc744b91c0e4a90ed125fb6ed19fada2caf63
Carla <carla.debeer.uk@gmail.com>
2017-01-04 13:52:16 +0000

    Function simplification

--- Multivariate Linear Regression/featureNormalize.m
+++ Multivariate Linear Regression/featureNormalize.m
@@ -5,15 +5,11 @@
 
 function [X_norm, mu, sigma] = featureNormalize(X)
 
-	X_norm = X;
-	mu = zeros(1, size(X, 2));
-	sigma = zeros(1, size(X, 2));     
+	mu = mean(X);
+	sigma = std(X);
 
-	mu = mean(X_norm);
-	sigma = std(X_norm);
-
-	tf_mu = X_norm - repmat(mu,length(X_norm),1);
-	tf_std = repmat(sigma,length(X_norm),1);
+	tf_mu = X - repmat(mu,length(X),1);
+	tf_std = repmat(sigma,length(X),1);
 
 	X_norm = tf_mu ./ tf_std;
 
